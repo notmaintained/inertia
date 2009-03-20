a185f22cea4f4883f8d084893dda8c717219b8b1
Sandeep Shetty <sandeep.shetty@gmail.com>
2009-03-20 10:52:08 +0530

    Removed Retest tests

--- tests/retest.test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	function test_retest_count_of_untested_lines()
-	{
-		should_return(8, when_passed(array('file1'=>array(1, 2), 'file2'=>array(1, 2, 3), 'file3'=>array(1, 2, 3))));
-	}
-
-?>
